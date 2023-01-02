d7c969f3272c6698351235d62e66a2efd7df8f76
user1 <user1@DESKTOP-MPUKNTA>
2023-01-02 17:25:17 +0900

    ADD day03 싱글톤

---
+++ src/com/kh/jdbc/day03/member/model/dao/MemberDAO.java
@@ -0,0 +1,114 @@
+package com.kh.jdbc.day03.member.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.kh.jdbc.day03.member.model.vo.Member;
+
+public class MemberDAO {
+	
+	// 회원 전체 정보 조회
+	public List<Member> selectAll(Connection conn) {
+		List<Member> mList = null;
+		String sql = "SELECT * FROM MEMBER_TBL";
+		try {
+			
+			Statement stmt = conn.createStatement();
+			ResultSet rset = stmt.executeQuery(sql);
+			mList = new ArrayList<Member>();
+			while(rset.next()) {
+				Member member = new Member();
+				
+				mList.add(member);
+			}
+			conn.close();
+			stmt.close();
+			rset.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return mList;
+	}
+	
+	public int insertMember(Connection conn, Member member) {
+		int result = 0;
+		String sql = "INSERT INTO MEMBER_TBL VALUES(?,?,?,?,?,?,?,?,?,DEFAULT)";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberId());
+			pstmt.setString(2, member.getMemberPwd());
+			pstmt.setString(3, member.getMemberName());
+			pstmt.setString(4, member.getMemberGender());
+			pstmt.setInt(5, member.getMemberAge());
+			pstmt.setString(6, member.getMemberEmail());
+			pstmt.setString(7, member.getMemberPhone());
+			pstmt.setString(8, member.getMemberAddress());
+			pstmt.setString(9, member.getMemberHobby());
+			result = pstmt.executeUpdate();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
+	public Member selectOneById(Connection conn, String memberId) {
+		Member member = null;
+		String sql = "SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setNString(1, memberId);  // 쿼리문 실행 준비
+			ResultSet rset = pstmt.executeQuery();
+			if(rset.next()) {
+				member = new Member();
+				member.setMemberId(rset.getString("MEMBER_ID"));
+				member.setMemberPwd(rset.getString("MEMBER_PWD"));
+				member.setMemberName(rset.getString("MEMBER_NAME"));
+				member.setMemberGender(rset.getString("MEMBER_GENDER"));
+				member.setMemberAge(rset.getInt("MEMBER_AGE"));
+				member.setMemberEmail(rset.getString("MEMBER_EMAIL"));
+				member.setMemberPhone(rset.getString("MEMBER_PHONE"));
+				member.setMemberAddress(rset.getString("MEMBER_ADDRESS"));
+				member.setMemberHobby(rset.getString("MEMBER_HOBBY"));
+				member.setMemberDate(rset.getTimestamp("MEMBER_DATE"));
+			}
+			rset.close();
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return member;
+	}
+	
+	public int updateMember(Connection conn, Member member) {  //회원 정보 수정
+		//Connection conn 얘는 서비스에서 , 
+		// UPDATE MEMBER_TBL SET MEMBER_PWD = ?, MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?
+		
+		// Class.forName(DRIVER_NAME);
+		// Connection conn = DreiverManager.getConnection(URL, USER, PASSWORD); 
+		// 이제 얘네를 여기서 안해도됨
+		String sql = "UPDATE MEMBER_TBL SET MEMBER_PWD = ?, MEMBER_EMAIL = ?, MEMBER_PHONE = ?, MEMBER_ADDRESS = ?, MEMBER_HOBBY = ? WHERE MEMBER_ID = ?";
+		int result = 0;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, member.getMemberPwd());
+			pstmt.setString(2, member.getMemberEmail());
+			pstmt.setString(3, member.getMemberPhone());
+			pstmt.setString(4, member.getMemberAddress());
+			pstmt.setString(5, member.getMemberHobby());
+			pstmt.setString(6, member.getMemberId());  // 쿼리문 실행 준비 완료
+			result = pstmt.executeUpdate(); 		// 쿼리문 실행
+			pstmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+}
---
+++ src/com/kh/jdbc/day03/member/view/MemberView.java
@@ -0,0 +1,137 @@
+package com.kh.jdbc.day03.member.view;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.kh.jdbc.day03.member.model.vo.Member;
+
+public class MemberView {
+	
+	public int mainMenu() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("=== === 회원관리 프로그램 === ===");
+		System.out.println("1. 회원 전체 조회");
+		System.out.println("2. 회원 아이디로 조회");
+		System.out.println("3. 회원 이름으로 조회");
+		System.out.println("4. 회원 가입");
+		System.out.println("5. 회원 정보 수정");
+		System.out.println("6. 회원 탈퇴");
+		System.out.println("0. 프로그램 종료");
+		System.out.print("메뉴 선택 : ");
+		int choice = sc.nextInt();
+		return choice;
+	}
+	
+	public void showAll(List<Member> mList) {
+		System.out.println("=== === 전체 회원 조회 결과 === ===");
+		for (Member mOne : mList) {
+			System.out.print("ID : " + mOne.getMemberId());
+			System.out.print(", 비밀번호 : " + mOne.getMemberPwd());
+			System.out.print(", 이름 : " + mOne.getMemberName());
+			System.out.print(", 성별 : " + mOne.getMemberGender());
+			System.out.print(", 나이 : " + mOne.getMemberAge());
+			System.out.print(", 이메일 : " + mOne.getMemberEmail());
+			System.out.print(", 번호 : " + mOne.getMemberPhone());
+			System.out.print(", 주소 : " + mOne.getMemberAddress());
+			System.out.print(", 취미 : " + mOne.getMemberHobby());
+			System.out.println(", 가입날짜 : " + mOne.getMemberDate());
+			// 마지막은 ln 안하면 === 회원관리 프로그램 === 이게 옆에 붙어서 나온다
+		}
+	}
+	
+	public void showOne(Member member) {
+		System.out.print("ID : " + member.getMemberId());
+		System.out.print(", 비밀번호 : " + member.getMemberPwd());
+		System.out.print(", 이름 : " + member.getMemberName());
+		System.out.print(", 성별 : " + member.getMemberGender());
+		System.out.print(", 나이 : " + member.getMemberAge());
+		System.out.print(", 이메일 : " + member.getMemberEmail());
+		System.out.print(", 번호 : " + member.getMemberPhone());
+		System.out.print(", 주소 : " + member.getMemberAddress());
+		System.out.print(", 취미 : " + member.getMemberHobby());
+		System.out.println(", 가입날짜 : " + member.getMemberDate());
+	}
+	
+	public Member modifyMember(Member member) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("수정할 비밀번호 입력 : ");
+		String memberPwd = sc.next();
+		System.out.println("수정할 이메일 입력 : ");
+		String memberEmail = sc.next();
+		System.out.println("수정할 번호 입력 : ");
+		String memberPhone = sc.next();
+		System.out.println("수정할 주소 입력 : ");
+		sc.nextLine();
+		String memberAddress = sc.nextLine();
+		System.out.println("수정할 취미 입력 : ");
+		String memberHobby = sc.next();
+		member.setMemberPwd(memberPwd);
+		member.setMemberEmail(memberEmail);
+		member.setMemberPhone(memberPhone);
+		member.setMemberAddress(memberAddress);
+		member.setMemberHobby(memberHobby);
+		return member;
+	}
+	
+	public Member inputMember() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("아이디 입력 : ");
+		String memberId = sc.next();
+		System.out.print("비밀번호 입력 : ");
+		String memberPwd = sc.next();
+		System.out.print("이름 입력 : ");
+		String memberName = sc.next();
+		System.out.print("성별 입력 : ");
+		String memberGender = sc.next();
+		System.out.print("나이 입력 : ");
+		int memberAge = sc.nextInt();
+		System.out.print("이메일 입력 : ");
+		String memberEmail = sc.next();
+		System.out.print("번호 입력 : ");
+		String memberPhone = sc.next();
+		System.out.print("주소 입력 : ");
+		sc.nextLine();
+		String memberAddress = sc.nextLine();
+		System.out.print("취미 입력 : ");
+		String memberHobby = sc.next();
+		Member member = new Member(memberId,memberPwd,memberName,memberGender,memberAge,
+				memberEmail,memberPhone,memberAddress,memberHobby,null);
+		return member;
+	}
+	
+	public String inputMemberName() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("이름 입력 : ");       // 가이드메시지 잊지말기
+		String memberName = sc.next();
+		return memberName;
+	}
+	
+	public Member inputLoginInfo() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("=== === 로그인 정보 입력 === ===");
+		System.out.print("아이디 : ");
+		String memberId = sc.next();
+		System.out.println("비밀번호 : ");
+		String memberPwd = sc.next();
+		Member member = new Member();
+		member.setMemberId(memberId);
+		member.setMemberPwd(memberPwd);
+		return member;
+	}
+	
+	public String inputMemberId(String message) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(message + "할 아이디 입력 : ");
+		String memberId = sc.next();
+		return memberId;
+	}
+	
+	public void displaySuccess(String message) {
+		System.out.println("[서비스 성공]" + message);
+	}
+	
+	public void displayError(String message) {
+		System.out.println("[서비스 실패]" + message);
+	}
+
+}
